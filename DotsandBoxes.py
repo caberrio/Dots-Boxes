654217206d7deb92c8ec89c68a0dabd136e50fa9
Carlos Díaz Berrio <carlosberrio08@hotmail.com>
2017-04-08 17:29:03 -0500

    Update DotsandBoxes.py

--- DotsandBoxes.py
+++ DotsandBoxes.py
@@ -1,6 +1,5 @@
 from tkinter import *
 
-
 root=Tk() 
 root.title("Dots & Boxes")
 root.resizable(width=False, height=False)
@@ -13,13 +12,195 @@ for j in range(0,6):
             for i in range(0,6):
                 canvas.create_oval(i*80+100, j*80+100, (i+1)*80, (j+1)*80, outline="blue",
                                 fill="blue", width=2)
+
+
 canvas.pack(fill=BOTH, expand=1)
 
-canvas.bind("<Button-1>", callback)
+
 
 def callback(event):
     print ("clicked at", event.x, event.y)
+    if(event.x > 80 and event.x < 100 and event.y > 80 and event.y < 140):
+        canvas.create_line(90, 100, 90, 160, fill="red", width=10)
+        
+    if(event.x > 80 and event.x < 100 and event.y > 160 and event.y < 220):
+        canvas.create_line(90, 180, 90, 240, fill="red", width=10)
+        
+    if(event.x > 80 and event.x < 100 and event.y > 240 and event.y < 300):
+        canvas.create_line(90, 260, 90, 320, fill="red", width=10)
+        
+    if(event.x > 80 and event.x < 100 and event.y > 320 and event.y < 380):
+        canvas.create_line(90, 340, 90, 400, fill="red", width=10)
+        
+    if(event.x > 80 and event.x < 100 and event.y > 400 and event.y < 460):
+        canvas.create_line(90, 420, 90, 480, fill="red", width=10)
+        
+    if(event.x > 160 and event.x < 180 and event.y > 80 and event.y < 140):
+        canvas.create_line(170, 100, 170, 160, fill="red", width=10)
+        
+    if(event.x > 160 and event.x < 180 and event.y > 160 and event.y < 220):
+        canvas.create_line(170, 180, 170, 240, fill="red", width=10)
+        
+    if(event.x > 160 and event.x < 180 and event.y > 240 and event.y < 300):
+        canvas.create_line(170, 260, 170, 320, fill="red", width=10)
+        
+    if(event.x > 160 and event.x < 180 and event.y > 320 and event.y < 380):
+        canvas.create_line(170, 340, 170, 400, fill="red", width=10)
+        
+    if(event.x > 160 and event.x < 180 and event.y > 400 and event.y < 460):
+        canvas.create_line(170, 420, 170, 480, fill="red", width=10)
+    
+    if(event.x > 240 and event.x < 260 and event.y > 80 and event.y < 140):
+        canvas.create_line(250, 100, 250, 160, fill="red", width=10)
+        
+    if(event.x > 240 and event.x < 260 and event.y > 160 and event.y < 220):
+        canvas.create_line(250, 180, 250, 240, fill="red", width=10)
+        
+    if(event.x > 240 and event.x < 260 and event.y > 240 and event.y < 300):
+        canvas.create_line(250, 260, 250, 320, fill="red", width=10)
+    
+    if(event.x > 240 and event.x < 260 and event.y > 320 and event.y < 380):
+        canvas.create_line(250, 340, 250, 400, fill="red", width=10)
+        
+    if(event.x > 240 and event.x < 260 and event.y > 400 and event.y < 460):
+        canvas.create_line(250, 420, 250, 480, fill="red", width=10)
+        
+    if(event.x > 320 and event.x < 340 and event.y > 80 and event.y < 140):
+        canvas.create_line(330, 100, 330, 160, fill="red", width=10)
+        
+    if(event.x > 320 and event.x < 340 and event.y > 160 and event.y < 220):
+        canvas.create_line(330, 180, 330, 240, fill="red", width=10)
+        
+    if(event.x > 320 and event.x < 340 and event.y > 240 and event.y < 300):
+        canvas.create_line(330, 260, 330, 320, fill="red", width=10)
+        
+    if(event.x > 320 and event.x < 340 and event.y > 320 and event.y < 380):
+        canvas.create_line(330, 340, 330, 400, fill="red", width=10)
+    
+    if(event.x > 320 and event.x < 340 and event.y > 400 and event.y < 460):
+        canvas.create_line(330, 420, 330, 480, fill="red", width=10)
+        
+    if(event.x > 400 and event.x < 420 and event.y > 80 and event.y < 140):
+        canvas.create_line(410, 100, 410, 160, fill="red", width=10)
+        
+    if(event.x > 400 and event.x < 420 and event.y > 160 and event.y < 220):
+        canvas.create_line(410, 180, 410, 240, fill="red", width=10)
+        
+    if(event.x > 400 and event.x < 420 and event.y > 240 and event.y < 300):
+        canvas.create_line(410, 260, 410, 320, fill="red", width=10)
+        
+    if(event.x > 400 and event.x < 420 and event.y > 320 and event.y < 380):
+        canvas.create_line(410, 340, 410, 400, fill="red", width=10)
+        
+    if(event.x > 400 and event.x < 420 and event.y > 400 and event.y < 460):
+        canvas.create_line(410, 420, 410, 480, fill="red", width=10)
+        
+    if(event.x > 480 and event.x < 500 and event.y > 80 and event.y < 140):
+        canvas.create_line(490, 100, 490, 160, fill="red", width=10)
+    
+    if(event.x > 480 and event.x < 500 and event.y > 160 and event.y < 220):
+        canvas.create_line(490, 180, 490, 240, fill="red", width=10)
+    
+    if(event.x > 480 and event.x < 500 and event.y > 240 and event.y < 300):
+        canvas.create_line(490, 260, 490, 320, fill="red", width=10)
+    
+    if(event.x > 480 and event.x < 500 and event.y > 320 and event.y < 380):
+        canvas.create_line(490, 340, 490, 400, fill="red", width=10)
+        
+    if(event.x > 480 and event.x < 500 and event.y > 400 and event.y < 460):
+        canvas.create_line(490, 420, 490, 480, fill="red", width=10)
+        
     if(event.x > 100 and event.x < 160 and event.y > 80 and event.y < 100):        
         canvas.create_line(100, 90, 160, 90, fill="red", width=10)
+        
+    if(event.x > 100 and event.x < 160 and event.y > 160 and event.y < 180):        
+        canvas.create_line(100, 170, 160, 170, fill="red", width=10)
+    
+    if(event.x > 100 and event.x < 160 and event.y > 240 and event.y < 260):
+        canvas.create_line(100, 250, 160, 250, fill="red", width=10)
+        
+    if(event.x > 100 and event.x < 160 and event.y > 320 and event.y < 340):
+        canvas.create_line(100, 330, 160, 330, fill="red", width=10)
+        
+    if(event.x > 100 and event.x < 160 and event.y > 400 and event.y < 420):
+        canvas.create_line(100, 410, 160, 410, fill="red", width=10)
+        
+    if(event.x > 100 and event.x < 160 and event.y > 480 and event.y < 500):
+        canvas.create_line(100, 490, 160, 490, fill="red", width=10)
+        
+    if(event.x > 180 and event.x < 240 and event.y > 80 and event.y < 100):        
+        canvas.create_line(180, 90, 240, 90, fill="red", width=10)
+        
+    if(event.x > 180 and event.x < 240 and event.y > 160 and event.y < 180):        
+        canvas.create_line(180, 170, 240, 170, fill="red", width=10)
+        
+    if(event.x > 180 and event.x < 240 and event.y > 240 and event.y < 260):
+        canvas.create_line(180, 250, 240, 250, fill="red", width=10)
 
+    if(event.x > 180 and event.x < 240 and event.y > 320 and event.y < 340):
+        canvas.create_line(180, 330, 240, 330, fill="red", width=10)
+
+    if(event.x > 180 and event.x < 240 and event.y > 400 and event.y < 420):
+        canvas.create_line(180, 410, 240, 410, fill="red", width=10)
+        
+    if(event.x > 180 and event.x < 240 and event.y > 480 and event.y < 500):
+        canvas.create_line(180, 490, 240, 490, fill="red", width=10)
+        
+    if(event.x > 260 and event.x < 320 and event.y > 80 and event.y < 100):        
+        canvas.create_line(260, 90, 320, 90, fill="red", width=10)
+        
+    if(event.x > 260 and event.x < 320 and event.y > 160 and event.y < 180):        
+        canvas.create_line(260, 170, 320, 170, fill="red", width=10)
+        
+    if(event.x > 260 and event.x < 320 and event.y > 240 and event.y < 260):
+        canvas.create_line(260, 250, 320, 250, fill="red", width=10)
+        
+    if(event.x > 260 and event.x < 320 and event.y > 320 and event.y < 340):
+        canvas.create_line(260, 330, 320, 330, fill="red", width=10)
+        
+    if(event.x > 260 and event.x < 320 and event.y > 400 and event.y < 420):
+        canvas.create_line(260, 410, 320, 410, fill="red", width=10)
+        
+    if(event.x > 260 and event.x < 320 and event.y > 480 and event.y < 500):
+        canvas.create_line(260, 490, 320, 490, fill="red", width=10)
+        
+    if(event.x > 340 and event.x < 400 and event.y > 80 and event.y < 100):        
+        canvas.create_line(340, 90, 400, 90, fill="red", width=10)
+        
+    if(event.x > 340 and event.x < 400 and event.y > 160 and event.y < 180):        
+        canvas.create_line(340, 170, 400, 170, fill="red", width=10)
+        
+    if(event.x > 340 and event.x < 400 and event.y > 240 and event.y < 260):
+        canvas.create_line(340, 250, 400, 250, fill="red", width=10)
+        
+    if(event.x > 340 and event.x < 400 and event.y > 320 and event.y < 340):
+        canvas.create_line(340, 330, 400, 330, fill="red", width=10)
+    
+    if(event.x > 340 and event.x < 400 and event.y > 400 and event.y < 420):
+        canvas.create_line(340, 410, 400, 410, fill="red", width=10)
+        
+    if(event.x > 340 and event.x < 400 and event.y > 480 and event.y < 500):
+        canvas.create_line(340, 490, 400, 490, fill="red", width=10)
+        
+    if(event.x > 420 and event.x < 480 and event.y > 80 and event.y < 100):        
+        canvas.create_line(420, 90, 480, 90, fill="red", width=10)
+        
+    if(event.x > 420 and event.x < 480 and event.y > 160 and event.y < 180):        
+        canvas.create_line(420, 170, 480, 170, fill="red", width=10)
+        
+    if(event.x > 420 and event.x < 480 and event.y > 240 and event.y < 260):
+        canvas.create_line(420, 250, 480, 250, fill="red", width=10)
+        
+    if(event.x > 420 and event.x < 480 and event.y > 320 and event.y < 340):
+        canvas.create_line(420, 330, 480, 330, fill="red", width=10)
+        
+    if(event.x > 420 and event.x < 480 and event.y > 400 and event.y < 420):
+        canvas.create_line(420, 410, 480, 410, fill="red", width=10)
+        
+    if(event.x > 420 and event.x < 480 and event.y > 480 and event.y < 500):
+        canvas.create_line(420, 490, 480, 490, fill="red", width=10)
+        
+
+
+canvas.bind("<Button-1>", callback)
 root.mainloop()
