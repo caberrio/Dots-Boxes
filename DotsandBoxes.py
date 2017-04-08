cffe41e928c347b62d422340defc419fb9b836da
Carlos Díaz Berrio <carlosberrio08@hotmail.com>
2017-04-08 15:13:53 -0500

    Update DotsandBoxes.py

--- DotsandBoxes.py
+++ DotsandBoxes.py
@@ -1,46 +1,25 @@
 from tkinter import *
-"""root=Tk()
+
+
+root=Tk() 
+root.title("Dots & Boxes")
 root.resizable(width=False, height=False)
-"""
-class Example(Frame):
- 
-    def __init__(self, parent):
-        Frame.__init__(self, parent)  
-         
-        self.parent = parent        
-        self.initUI()
-       
-       
-    def initUI(self):
-     
-        self.parent.title("Dots & Boxes")        
-        self.pack(fill=BOTH, expand=1)
-       
-        canvas = Canvas(self)
-        canvas.bind("<Key>", key)
-        canvas.bind("<Button-1>", callback)
-        for j in range(0,6):
+root.geometry("600x600+30+30")
+
+
+canvas = Canvas(root)
+
+for j in range(0,6):
             for i in range(0,6):
                 canvas.create_oval(i*80+100, j*80+100, (i+1)*80, (j+1)*80, outline="blue",
                                 fill="blue", width=2)
+canvas.pack(fill=BOTH, expand=1)
 
-        canvas.pack(fill=BOTH, expand=1)
-def key(event):
-    print ("pressed", repr(event.char))
+canvas.bind("<Button-1>", callback)
 
 def callback(event):
     print ("clicked at", event.x, event.y)
+    if(event.x > 100 and event.x < 160 and event.y > 80 and event.y < 100):        
+        canvas.create_line(100, 90, 160, 90, fill="red", width=10)
 
-
- 
-def main():
- 
-    root = Tk()
-    ex = Example(root)
-    root.geometry("600x600+30+30")
-    root.mainloop()  
- 
- 
-if __name__ == '__main__':
-
-        main()
+root.mainloop()
